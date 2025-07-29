f9526e4809158ca76fec64192255062f32ecfbbb
timmypelumy <timileyinpelumi06@gmail.com>
2025-07-29 23:56:55 +0100

    updates

---
+++ contracts/ignition/modules/ModulsSalesManager.js
@@ -0,0 +1,14 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+export default buildModule("ModulsSalesManager", (m) => {
+
+    const modulsSalesManager = m.contract("ModulsSalesManager", []);
+
+
+
+    return { modulsSalesManager };
+});
