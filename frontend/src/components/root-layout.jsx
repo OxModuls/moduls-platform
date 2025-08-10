d7a80b77fab59c2a824927d71ee0adb5d186f27a
Quwam Ogunlaja <quwam32@gmail.com>
2025-08-10 06:53:42 +0100

    move WalletConnectModal to root layout component

--- frontend/src/components/root-layout.jsx
+++ frontend/src/components/root-layout.jsx
@@ -2,20 +2,25 @@ import { Outlet } from "react-router";
 import Header from "./header";
 import Footer from "./footer";
 import { Toaster } from "./ui/sonner";
+import WalletConnectModal from "./wallet-connect-modal";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 const RootLayout = () => {
+  const isMobile = useIsMobile();
+
   return (
-    <div className="w-full text-primary bg-background">
-      <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
-        <Header />
+    <div className="w-full bg-background text-primary">
+      <div className="grid min-h-screen grid-rows-[auto_1fr_auto]">
+        <Header className="z-10" />
         <main>
           <Outlet />
         </main>
         <Footer />
       </div>
-      <Toaster position="top-right"  />
+      <Toaster position={isMobile ? "top-right" : "bottom-right"} />
+      <WalletConnectModal />
     </div>
   );
 };
 
-export default RootLayout; 
+export default RootLayout;
