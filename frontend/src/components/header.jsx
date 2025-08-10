16a0f89957143fc193bb1f999dcf3304f01e07e6
Quwam Ogunlaja <quwam32@gmail.com>
2025-08-10 07:13:22 +0100

    update header comopnent

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -7,41 +7,40 @@ import SearchBar from "./search-bar";
 import { Menu } from "lucide-react";
 import { useState } from "react";
 import MenuSheet from "./menu-sheet";
+import { cn } from "../lib/utils";
 
-const Header = () => {
+const Header = ({ className }) => {
   const pathname = useLocation().pathname;
   const [menuSheetOpen, setMenuSheetOpen] = useState(false);
 
   return (
-    <>
-      <header className="w-full px-4 pb-4">
-        <div className="flex items-center justify-between py-4">
-          <div className="flex items-center gap-2">
-            <Link to="/" className="flex items-center gap-2">
-              <img src={modulsLogo} alt="Moduls Logo" className="h-10" />
-            </Link>
-          </div>
-          <div className="flex items-center gap-4">
-            <div className="hidden w-80 md:block">
-              <SearchBar placeholder="Search agents, tokens..." />
-            </div>
-            <ConnectWalletButton />
-            <button
-              onClick={() => setMenuSheetOpen((prev) => !prev)}
-              className="cursor-pointer"
-            >
-              <Menu className="size-7" />
-            </button>
-          </div>
+    <header className={cn("w-full px-4 pb-4", className)}>
+      <div className="flex items-center justify-between py-4">
+        <div className="flex items-center gap-2">
+          <Link to="/" className="flex items-center gap-2">
+            <img src={modulsLogo} alt="Moduls Logo" className="h-10" />
+          </Link>
         </div>
-        {pathname === "/" && (
-          <div className="px-2 md:hidden">
+        <div className="flex items-center gap-4">
+          <div className="hidden w-80 md:block">
             <SearchBar placeholder="Search agents, tokens..." />
           </div>
-        )}
-      </header>
+          <ConnectWalletButton />
+          <button
+            onClick={() => setMenuSheetOpen((prev) => !prev)}
+            className="cursor-pointer"
+          >
+            <Menu className="size-7" />
+          </button>
+        </div>
+      </div>
+      {pathname === "/" && (
+        <div className="px-2 md:hidden">
+          <SearchBar placeholder="Search agents, tokens..." />
+        </div>
+      )}
       <MenuSheet open={menuSheetOpen} onOpenChange={setMenuSheetOpen} />
-    </>
+    </header>
   );
 };
 
